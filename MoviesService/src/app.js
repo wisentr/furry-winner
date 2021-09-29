a5529fc5bacdd4802ab2b98a1cd0b2f0e946b9e2
wisentr <mehmetefeekiner@gmail.com>
2021-09-29 11:10:09 +0200

    Seperate start listening and setting up the express server for testing purposes

---
+++ MoviesService/src/app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const checkAuth = require("../middleware/checkAuth.middleware");
+const moviesController = require("../controllers/movies.controllers");
+
+const app = express();
+app.use(express.json());
+
+// On each request, verify the authorization header, if it fails, do not go any further
+app.all("/movies", checkAuth);
+
+app.get("/movies", moviesController.getMovies);
+
+app.post("/movies", moviesController.postMovies);
+
+module.exports = app;
--- MoviesService/src/server.js
+++ MoviesService/src/server.js
@@ -1,18 +1,5 @@
-require("dotenv").config();
-const express = require("express");
-const checkAuth = require("../middleware/checkAuth.middleware");
-const moviesController = require("../controllers/movies.controllers");
+const app = require("./app");
 const PORT = 3001;
-
-const app = express();
-app.use(express.json());
-
-// On each request, verify the authorization header, if it fails, do not go any further
-app.all("/movies", checkAuth);
-
-app.get("/movies", moviesController.getMovies);
-
-app.post("/movies", moviesController.postMovies);
 
 app.listen(PORT, () => {
   console.log(`MoviesService is running at port ${PORT}`);
